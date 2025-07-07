f30c7714f3690d7d43e54206ed354fd89da7a2f7
lpraat <pratissolil@gmail.com>
2025-07-07 23:46:25 +0200

    Fix clang formatting for VEC_NAME(T)

--- src/vec.h
+++ src/vec.h
@@ -29,14 +29,14 @@ struct VEC_NAME(T) {
 #endif
 
 #ifndef NO_VEC_IMPL
-void METHOD_NAME(reserve)(VEC_NAME(T) * vec, size_t additional_capacity) {
+void METHOD_NAME(reserve)(VEC_NAME(T)* vec, size_t additional_capacity) {
     vec->data = vec->allocator->realloc(
         vec->allocator, vec->data,
         (vec->capacity + additional_capacity) * sizeof(T)
     );
 }
 
-void METHOD_NAME(push)(VEC_NAME(T) * vec, T elem) {
+void METHOD_NAME(push)(VEC_NAME(T)* vec, T elem) {
     if (vec->len + 1 > vec->capacity) {
         const size_t new_capacity = vec->capacity ? vec->capacity * 2 : 1;
         METHOD_NAME(reserve)(vec, new_capacity - vec->capacity);
@@ -52,12 +52,12 @@ void METHOD_NAME(push)(VEC_NAME(T) * vec, T elem) {
     vec->len += 1;
 }
 
-T METHOD_NAME(pop)(VEC_NAME(T) * vec) {
+T METHOD_NAME(pop)(VEC_NAME(T)* vec) {
     vec->len--;
     return vec->data[vec->len];
 }
 
-void METHOD_NAME(extend)(VEC_NAME(T) * vec, VEC_NAME(T) * other) {
+void METHOD_NAME(extend)(VEC_NAME(T)* vec, VEC_NAME(T)* other) {
     if (vec->capacity - vec->len < other->len) {
         METHOD_NAME(reserve)(vec, other->len - (vec->capacity - vec->len));
         if (!vec->data) {
@@ -70,10 +70,8 @@ void METHOD_NAME(extend)(VEC_NAME(T) * vec, VEC_NAME(T) * other) {
     );
 }
 
-VEC_NAME(T)
-METHOD_NAME(new)(Allocator* a) {
-    VEC_NAME(T)
-    t = {
+VEC_NAME(T) METHOD_NAME(new)(Allocator* a) {
+    VEC_NAME(T) t = {
         .allocator = a,
         .data = 0,
         .capacity = 0,
@@ -82,10 +80,8 @@ METHOD_NAME(new)(Allocator* a) {
     return t;
 }
 
-VEC_NAME(T)
-METHOD_NAME(new_with_capacity)(size_t capacity) {
-    VEC_NAME(T)
-    t = {0};
+VEC_NAME(T) METHOD_NAME(new_with_capacity)(size_t capacity) {
+    VEC_NAME(T) t = {0};
     t.capacity = capacity;
     return t;
 }
